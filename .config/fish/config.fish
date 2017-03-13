528c476ac54b04a75bf5ec11de259be4e395597e
Stein Martin Hustad <stein@hustad.com>
2017-03-13 23:01:07 +0100

    chore(fish): Initialise base16 theme if necessary

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,9 @@ if status --is-interactive
   set -g EDITOR nvim
   set -g FZF_TMUX 1
 
+  if not test -f ~/.base16_theme
+    base16 ocean
+  end
   sh ~/.base16_theme
 
   command --search nodenv >> /dev/null; and source (nodenv init -|psub)
