d8ce9b24b97487768bd7b88f4cfcfe2daaca115e
Stein Martin Hustad <stein@hustad.com>
2021-11-04 17:52:35 +0400

    asdf setup fix

--- .config/fish/conf.d/asdf.fish
+++ .config/fish/conf.d/asdf.fish
@@ -2,4 +2,7 @@ set -x ASDF_DATA_DIR ~/.config/asdf
 
 if test -d $ASDF_DATA_DIR
     source $ASDF_DATA_DIR/asdf.fish
+    if test -d $ASDF_DATA_DIR/plugins/java
+        source $ASDF_DATA_DIR/plugins/java/set-java-home.fish
+    end
 end
