941f27d27b3e382708da0185f5515f0b2a4154c0
Stein Martin Hustad <stein@hustad.com>
2021-12-08 21:58:38 +0400

    switch to brew asdf

--- .config/fish/conf.d/asdf.fish
+++ .config/fish/conf.d/asdf.fish
@@ -1,8 +1,14 @@
-set -x ASDF_DATA_DIR ~/.config/asdf
+source (brew --prefix asdf)/libexec/asdf.fish
 
-if test -d $ASDF_DATA_DIR
-    source $ASDF_DATA_DIR/asdf.fish
-    if test -d $ASDF_DATA_DIR/plugins/java
-        source $ASDF_DATA_DIR/plugins/java/set-java-home.fish
-    end
+if test -d $ASDF_DATA_DIR/plugins/java
+   source $ASDF_DATA_DIR/plugins/java/set-java-home.fish
 end
+
+#set -x ASDF_DATA_DIR ~/.config/asdf
+#
+#if test -d $ASDF_DATA_DIR
+#    source $ASDF_DATA_DIR/asdf.fish
+#    if test -d $ASDF_DATA_DIR/plugins/java
+#        source $ASDF_DATA_DIR/plugins/java/set-java-home.fish
+#    end
+#end
