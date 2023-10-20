948e98e4356c953e90ec465f7ce6013d8e67e0b6
Stein Martin Hustad <stein@hustad.com>
2023-10-20 09:13:45 +0400

    Add prettier to neovim

--- .config/nvim/lua/smh/packer.lua
+++ .config/nvim/lua/smh/packer.lua
@@ -249,6 +249,9 @@ return packer.startup(function(use)
   -- }
   -- use 'nvim-telescope/telescope-fzy-native.nvim'
 
+
+  use('MunifTanjim/prettier.nvim')
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if is_bootstrap then
